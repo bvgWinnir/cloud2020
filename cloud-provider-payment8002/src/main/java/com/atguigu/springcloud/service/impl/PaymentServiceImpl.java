f498b7507b7f69b31a76c15f73d13f8d8f853ce7
bvgwinnir <bvg_winnir@163.com>
2020-03-13 22:41:16 +0800

    于8001 上一版同步

---
+++ cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,89 @@
+package com.atguigu.springcloud.service.impl;
+
+import com.atguigu.springcloud.dao.PaymentDao;
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * (Payment)表服务实现类
+ *
+ * @author makejava
+ * @since 2020-03-11 14:56:48
+ */
+@Slf4j
+@Service("paymentService")
+public class PaymentServiceImpl implements PaymentService {
+    @Autowired
+    private PaymentDao paymentDao;
+
+    /**
+     * 通过ID查询单条数据
+     *
+     * @param id 主键
+     * @return 实例对象
+     */
+    @Override
+    public Payment queryById(Long id) {
+        return this.paymentDao.queryById(id);
+    }
+
+    /**
+     * 查询多条数据
+     *
+     * @param offset 查询起始位置
+     * @param limit 查询条数
+     * @return 对象列表
+     */
+    @Override
+    public List<Payment> queryAllByLimit(int offset, int limit) {
+        return this.paymentDao.queryAllByLimit(offset, limit);
+    }
+
+    /**
+     * 新增数据
+     *
+     * @param payment 实例对象
+     * @return 实例对象
+     */
+    @Override
+    public Payment insert(Payment payment) {
+        this.paymentDao.insert(payment);
+        return payment;
+    }
+@Override
+    public CommonResult create(Payment payment){
+        int insert = paymentDao.insert(payment);
+        log.info("添加成功-----------------------------------------------------");
+        if (insert>0) return new CommonResult(200,"插入数据库成功",insert);
+        else return new CommonResult(500,"error");
+    }
+
+    /**
+     * 修改数据
+     *
+     * @param payment 实例对象
+     * @return 实例对象
+     */
+    @Override
+    public Payment update(Payment payment) {
+        this.paymentDao.update(payment);
+        return this.queryById(payment.getId());
+    }
+
+    /**
+     * 通过主键删除数据
+     *
+     * @param id 主键
+     * @return 是否成功
+     */
+    @Override
+    public boolean deleteById(Long id) {
+        return this.paymentDao.deleteById(id) > 0;
+    }
+}
